3da25b4baae2980393d5831dc11d77cd9f03a731
Pubudu Dodangoda <pubudu.dodangoda@trycake.com>
2017-08-27 20:42:56 +0530

    Update configurations

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ let gulp = require("gulp");
 let babel = require("gulp-babel");
 
 gulp.task("babel", () =>
-    gulp.src("src/**/*.js")
+    gulp.src(["src/**/*.js", "!src/**/*.test.js"])
         .pipe(babel())
         .pipe(gulp.dest("dist"))
 );
